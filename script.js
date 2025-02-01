ad31765e761e6ba4089e18bc5871850a90428c2e
Oksana <ksushatarasenko@gmail.com>
2025-02-01 07:51:50 +0100

    new files

---
+++ script.js
@@ -0,0 +1,6 @@
+// кнопка для подсказки --------------------------------------------
+
+function toggleHint2(container) {
+    // Добавление/удаление класса `active`
+    container.classList.toggle('active');
+}
