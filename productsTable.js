dbd1edcd541284d2e36d2fef745e61e3e1d0a270
Oksana <ksushatarasenko@gmail.com>
2026-02-16 07:08:44 +0100

    new

--- productsTable.js
+++ productsTable.js
@@ -1,13 +1,21 @@
 document.getElementById("search").addEventListener("input", function () {
   const filter = this.value.toLowerCase();
-  const rows = document.querySelectorAll("#productsTable tr");
 
-  rows.forEach((row, index) => {
-    if (index === 0) return; // пропуск заголовка
+  // все таблицы на странице
+  const tables = document.querySelectorAll("table");
 
-    const cell = row.querySelector("td");
-    const text = cell.textContent.toLowerCase();
+  tables.forEach(table => {
+    const rows = table.querySelectorAll("tr");
 
-    row.style.display = text.includes(filter) ? "" : "none";
+    rows.forEach((row, index) => {
+      if (index === 0) return; // пропуск заголовка
+
+      const cell = row.querySelector("td");
+      if (!cell) return;
+
+      const text = cell.textContent.toLowerCase();
+
+      row.style.display = text.includes(filter) ? "" : "none";
+    });
   });
 });
